bde8ab48364cd344a4e4d912d171147f333fe889
(s) Oktawian Klosko <oklosko@pm7-0065.uopnet.plymouth.ac.uk>
2017-12-06 11:32:19 +0000

    Started working on JS

--- sketch.js
+++ sketch.js
@@ -3,11 +3,11 @@ function setup() {
   var canvas = createCanvas(500, 500);
   canvas.parent("myContainer");
 
-  //var text = createDiv('GAD405-Creative Coding');
+  var text = createDiv('GAD405-Creative Coding');
 
   text.position(50,50);
 
-  frameRate(8);
+  frameRate(10);
 
   x = random(width);
   y = random(height);
@@ -16,6 +16,18 @@ function setup() {
 
 function draw() {
   background(160);
-  }
+
+  for(let i=0; i<100; i++){
+push();
+if (mouseIsPressed){
+
+translate(10,10);
+fill(random(255), random(255), random(255));
+noStroke();
+ellipse(random(width), random(height), random(50), random(50));
+pop();
+
+}
+}
 
 }
