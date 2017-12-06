8c40f6b549c5186d747e71ea16d0895e3439b22c
(s) Oktawian Klosko <oklosko@pm7-0065.uopnet.plymouth.ac.uk>
2017-12-06 12:36:33 +0000

    Added more detail to JS, Need to create arrays to change multiple shapes

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 
 function setup() {
-  var canvas = createCanvas(500, 500);
+  var canvas = createCanvas(594, 841);
   canvas.parent("myContainer");
 
   var text = createDiv('GAD405-Creative Coding');
@@ -11,23 +11,20 @@ function setup() {
 
   x = random(width);
   y = random(height);
-
 }
 
 function draw() {
-  background(160);
+  //background(160);
 
   for(let i=0; i<100; i++){
-push();
-if (mouseIsPressed){
-
-translate(10,10);
-fill(random(255), random(255), random(255));
-noStroke();
-ellipse(random(width), random(height), random(50), random(50));
-pop();
-
-}
-}
-
+    push();
+    if (mouseIsPressed){
+      translate(10,10);
+      fill(random(255), random(255), random(255), (200));
+      noStroke();
+      //ellipse(random(width), random(height), random(10), random(10));
+      rect(random(width), random(height), random(10), random(10));
+      pop();
+    }
+  }
 }
