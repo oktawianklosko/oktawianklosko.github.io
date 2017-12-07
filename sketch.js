f978ea115a8acb7e80f964c1a2c7f5373556d302
(s) Oktawian Klosko <oklosko@pm7-0065.uopnet.plymouth.ac.uk>
2017-12-07 13:53:35 +0000

    Added feature to change colour depending on mouse position

--- sketch.js
+++ sketch.js
@@ -13,17 +13,40 @@ function setup() {
   y = random(height);
 }
 
+
 function draw() {
   //background(160);
+
+  if (mouseX,mouseY) {
+        r = (255);
+        g = (255);
+        b = (255);
+
+if (mouseX < 594 & mouseY < 841){
+fill(r,g,b, mouseX,mouseY);
+}
+if (mouseX < 594 & mouseY < 841){
+fill(b, mouseX,mouseY);
+}
+
+if (mouseX < 594 & mouseY < 841){
+fill(r, mouseX,mouseY);
+}
+if (mouseX < 594 & mouseY < 841){
+fill(g, mouseX,mouseY);
+
+}
+}
 
   for(let i=0; i<100; i++){
     push();
     if (mouseIsPressed){
       translate(10,10);
-      fill(random(255), random(255), random(255), (200));
-      noStroke();
+      //fill(random(255), random(0), random(20), (200));
+      noStroke(); 
       rect(random(width), random(height), random(50), random(50));
       pop();
-    }
+
   }
+}
 }
