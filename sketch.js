89e834d43e6a86d6d1da1aee21cf587082d3eb16
(s) Oktawian Klosko <oklosko@pm7-0065.uopnet.plymouth.ac.uk>
2017-12-07 16:16:41 +0000

    First attempt at adding mouse release feature

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 
 function setup() {
+  //Size of the canvas
   var canvas = createCanvas(594, 841);
   canvas.parent("myContainer");
 
@@ -13,15 +14,16 @@ function setup() {
   y = random(height);
 }
 
-
+var value = r, g, b;
 function draw() {
   //background(160);
 
+//Changes color of shapes depending on the position of the mouse on the canvas
   if (mouseX,mouseY) {
-        r = (254);
-        g = (254);
-        b = (254);
-
+        r = (255);
+        g = (255);
+        b = (255);
+//These lines control the coordinates of the canvas and as well as the color
 if (mouseX < 594 & mouseY < 841){
 fill(r,g,b, mouseX,mouseY);
 }
@@ -34,19 +36,26 @@ fill(r, mouseX,mouseY);
 }
 if (mouseX < 594 & mouseY < 841){
 fill(g, mouseX,mouseY);
-
 }
 }
-
+//Creates a Forloop which makes shapes when the mouse is pressed
   for(let i=0; i<90; i++){
     push();
     if (mouseIsPressed){
       translate(0, 0);
       stroke(0);
+      scale(1);
       rect(random(width), random(height), random(50), random(50));
       //fill(random(255), random(0), random(20), (200));
       pop();
-
   }
 }
+}
+
+function mouseReleased(){
+  if (value == 255) {
+    value = 0;
+  } else {
+    value = 0;
+  }
 }
