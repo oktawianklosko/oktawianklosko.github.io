965b0cabbcda6d1c789fbe595083edaa09de1ae1
oktawianklosko <oktawianklosko@gmail.com>
2017-12-10 12:21:27 +0000

    Attempt at adding random bouncing feature

--- sketch.js
+++ sketch.js
@@ -1,23 +1,38 @@
+let value = 0;
+let x2;
+let y2;
+let speedX;
+let speedY;
 
 function setup() {
   //Size of the canvas
   var canvas = createCanvas(594, 841);
   canvas.parent("myContainer");
-
-  var text = createDiv('GAD405-Creative Coding');
-
+  var text = createDiv('');
   text.position(50,50);
   background(160);
-
   frameRate(30);
-
   x = random(width);
   y = random(height);
+
+  x2 = width/2;
+  y2 = height/2;
+  speedX = random(-5., 5.);
+  speedY = random(-5., 5.);
+
 }
 
-var value = 0;
 function draw() {
-  //background(160);
+
+  x2 += speedX;
+  y2 += speedY;
+
+  if (x2>594) x2 = -25;
+
+if (x2<-25) x2 = 594;
+
+if (y2>841) y2 = -25;
+if (y2<-25) y2 = 841;
   //Changes color of shapes depending on the position of the mouse on the canvas
   if (mouseX,mouseY) {
     r = (value);
@@ -46,12 +61,11 @@ function draw() {
   for(let i=0; i<100; i++){
     push();
     if (mouseIsPressed){
-      //translate(0, 0);
       stroke(0);
       scale(1);
       let xPos = random(mouseX-newSize, mouseX+newSize);
       let yPos = random(mouseY-newSize, mouseY+newSize);
-      rect(mouseX, mouseY, random(100), random(100));
+      rect(x2, y2, random(100), random(100));
       //line(xPos, yPos, width/2, height/2, (255));
       //fill(random(255), random(0), random(20), (200));
       pop();
